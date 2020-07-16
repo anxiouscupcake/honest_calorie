0aa4f26093bb0fa1402ed89422e28ce69da9ba15
Roman Zubin <vivacer2@gmail.com>
2020-07-16 21:50:15 +0300

    empty values are no longer written to json file;

--- lib/types/product.dart
+++ lib/types/product.dart
@@ -1,17 +1,17 @@
 // Copyright (C) 2020 Roman Zubin
-// 
+//
 // This file is part of Honest Calorie.
-// 
+//
 // Honest Calorie is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // Honest Calorie is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Honest Calorie.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -52,23 +52,51 @@ class Product {
   int sodium = 0; // in mg
   double fibre = 0;
 
-  Product.fromJson(Map<String, dynamic> json)
-      // basic information
-      : name = json["name"],
-        category = json["category"],
-        calories = json["calories"],
-        servingSize = json["servingSize"],
-        servingUnit = json["servingUnit"],
-        // additional information
-        notes = json["notes"],
-        protein = json["protein"],
-        fat = json["fat"],
-        saturatedFat = json["saturatedFat"],
-        carbohydrates = json["carbohydrates"],
-        sugar = json["sugar"],
-        salt = json["salt"],
-        sodium = json["sodium"],
-        fibre = json["fibre"];
+  Product.fromJson(Map<String, dynamic> json) {
+    // basic information
+    name = json["name"];
+    category = json["category"];
+    calories = json["calories"];
+    servingSize = json["servingSize"];
+    servingUnit = json["servingUnit"];
+    // additional information
+    if (json["notes"] != null)
+      notes = json["notes"];
+    else
+      notes = "";
+    if (json["protein"] != null)
+      protein = json["protein"];
+    else
+      protein = 0;
+    if (json["fat"] != null)
+      fat = json["fat"];
+    else
+      fat = 0;
+    if (json["saturatedFat"] != null)
+      saturatedFat = json["saturatedFat"];
+    else
+      saturatedFat = 0;
+    if (json["carbohydrates"] != null)
+      carbohydrates = json["carbohydrates"];
+    else
+      carbohydrates = 0;
+    if (json["sugar"] != null)
+      sugar = json["sugar"];
+    else
+      sugar = 0;
+    if (json["salt"] != null)
+      salt = json["salt"];
+    else
+      salt = 0;
+    if (json["sodium"] != null)
+      sodium = json["sodium"];
+    else
+      sodium = 0;
+    if (json["fibre"] != null)
+      fibre = json["fibre"];
+    else
+      fibre = 0;
+  }
 
   Map<String, dynamic> toJson() => {
         // basic information
@@ -78,14 +106,23 @@ class Product {
         "servingSize": servingSize,
         "servingUnit": servingUnit,
         // additional information
-        "notes": notes,
-        "protein": protein,
-        "fat": fat,
-        "saturatedFat": saturatedFat,
-        "carbohydrates": carbohydrates,
-        "sugar": sugar,
-        "salt": salt,
-        "sodium": sodium,
-        "fibre": fibre,
+        if (notes != "")
+          "notes": notes,
+        if (protein != 0)
+          "protein": protein,
+        if (fat != 0)
+          "fat": fat,
+        if (saturatedFat != 0)
+          "saturatedFat": saturatedFat,
+        if (carbohydrates != 0)
+          "carbohydrates": carbohydrates,
+        if (sugar != 0)
+          "sugar": sugar,
+        if (salt != 0)
+          "salt": salt,
+        if (sodium != 0)
+          "sodium": sodium,
+        if (fibre != 0)
+          "fibre": fibre,
       };
 }
