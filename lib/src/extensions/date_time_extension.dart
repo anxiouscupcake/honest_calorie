ad6adc83322c2a96cdc5163e4e576346d05e98d3
Roman Zubin <utkabulka@gmail.com>
2024-03-07 16:00:53 +0400

    datetime extension

---
+++ lib/src/extensions/date_time_extension.dart
@@ -0,0 +1,54 @@
+extension DateTimeExtension on DateTime {
+  String toTimeAgo({bool numericDates = true}) {
+    final difference = DateTime.now().difference(this);
+
+    if ((difference.inDays / 7).floor() >= 1) {
+      return (numericDates) ? '1 week ago' : 'Last week';
+    } else if (difference.inDays >= 2) {
+      return '${difference.inDays} days ago';
+    } else if (difference.inDays >= 1) {
+      return (numericDates) ? '1 day ago' : 'Yesterday';
+    } else if (difference.inHours >= 2) {
+      return '${difference.inHours} hours ago';
+    } else if (difference.inHours >= 1) {
+      return (numericDates) ? '1 hour ago' : 'An hour ago';
+    } else if (difference.inMinutes >= 2) {
+      return '${difference.inMinutes} minutes ago';
+    } else if (difference.inMinutes >= 1) {
+      return (numericDates) ? '1 minute ago' : 'A minute ago';
+    } else if (difference.inSeconds >= 3) {
+      return '${difference.inSeconds} seconds ago';
+    } else {
+      return 'Just now';
+    }
+  }
+
+  String toReadableDay() {
+    final Duration difference = DateTime.now().difference(this);
+
+    if (difference.inDays == 0) {
+      return 'Today';
+    }
+    if (difference.inDays == 1) {
+      return 'Yesterday';
+    }
+
+    switch (weekday) {
+      case 1:
+        return 'Monday';
+      case 2:
+        return 'Tuesday';
+      case 3:
+        return 'Wednesday';
+      case 4:
+        return 'Thursday';
+      case 5:
+        return 'Friday';
+      case 6:
+        return 'Saturday';
+      case 7:
+      default:
+        return 'Sunday';
+    }
+  }
+}
